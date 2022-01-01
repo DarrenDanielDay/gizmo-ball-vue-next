0a507955f8467c2a5ec2ca87ee8ea3cd7adf34a9
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2022-01-01 20:19:22 +0800

    ci: Add Github Actions.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx';
+const DEPLOY_BASE_URL = process.env.DEPLOY_BASE_URL
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()]
+  plugins: [vue(), vueJsx()],
+  base: DEPLOY_BASE_URL ?? '/dist'
 })
